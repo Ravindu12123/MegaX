1e2d7424a9ecc8ccdcd87d3263e07a8b1dff2fc3
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2024-02-22 06:53:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.post('/p', cors(corsOptions), function (req, res, next) {
+app.post('/p', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for only example.com.'})
 })
 
